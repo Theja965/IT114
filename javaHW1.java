27ddb60d66fd78f5ca723298babbc4612fa12677
thejathondapi <bt248@njit.edu>
2020-09-14 16:20:32 -0400

    Java HW

---
+++ javaHW1.java
@@ -0,0 +1,13 @@
+public class javaHW1
+{
+	public static void main(String args[])
+	{
+	String name ="Theja";
+	System.out.println(name);
+	int age = 19;
+	System.out.println(name+"is"+age+"years old!");
+	double height = 185.4;
+	System.out.println(name+"is"+height+"Centimeters tall");
+	}
+} 
+	
